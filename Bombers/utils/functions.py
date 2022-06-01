1e3f28c9dacd43ca394ffb5105a3b39160cd79bf
Husniddin Murodov <73847672+hoosnick@users.noreply.github.com>
2022-06-01 10:23:01 +0500

    Added super_input() func.

--- Bombers/utils/functions.py
+++ Bombers/utils/functions.py
@@ -1,9 +1,12 @@
+import sys
 import os
 import platform
 
 import random
 
 from fake_useragent import UserAgent
+
+from typing import Union
 
 ua = UserAgent()
 
@@ -31,3 +34,40 @@ def random_user_agent() -> str:
 
 def all_bombers() -> list:
     return [f for f in os.listdir('Bombers/') if f.endswith(".py")]
+
+
+def super_input(**kwargs) -> Union[str, int, None]:
+    from .decorations import FormattingOptions
+
+    fo = FormattingOptions()
+    i = None
+    while True:
+        try:
+            value = input(kwargs['text'])
+            if value.lower() in ['exit', 'back', 'chqish', 'quit']:
+                clear_terminal()
+                sys.exit()
+            if kwargs['phone']:
+                num = value
+                i = num.replace('+998', '') \
+                    if num.startswith('+998') \
+                    else num
+                if i.isdigit() and len(i) == 9:
+                    break
+                print(fo._(fo.FAIL, "Xato kiritildi!"))
+                continue
+            elif kwargs['amount']:
+                amount = int(value)
+                i = amount \
+                    if amount <= 1000 and amount >= 10 \
+                    else 10
+                break
+        except ValueError:
+            print(fo._(fo.FAIL, "Faqat son yozish kerak!")); continue
+        except Exception as e:
+            print('Siz ssenariyda bo\'lmagan xatolikka yo\'l qo\'ydingiz.\n'
+                    'Ana endi mana bu xatolikni tarjima qilib, to\'g\'ri javob bering!\n'
+                    f'{e}')
+            break
+
+    return i
